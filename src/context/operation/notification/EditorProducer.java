bded10c136fd625df0b982ae3e73a17fdccdb148
Dan <danya.dyakonov@mail.ru>
2025-03-29 19:04:35 +0300

    add new row operation + notification refactor (not committed file)

---
+++ src/context/operation/notification/EditorProducer.java
@@ -0,0 +1,72 @@
+package context.operation.notification;
+
+import common.AsciiConstant;
+import common.PrimitiveOperation;
+import context.dto.ContextCursorNotificationModel;
+import context.dto.ContextRowNotificationModel;
+import context.dto.RowContentModel;
+import context.operation.state.EditorState;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class EditorProducer implements Producer {
+
+    private final Collection<Consumer> consumers;
+
+    public EditorProducer() {
+        this.consumers = new HashSet<>();
+    }
+
+    public EditorProducer(Consumer consumer) {
+        this.consumers = new HashSet<>();
+        consumers.add(consumer);
+    }
+
+    @Override
+    public void attachConsumer(Consumer consumer) {
+        consumers.add(consumer);
+    }
+
+    @Override
+    public void detachConsumer(Consumer consumer) {
+        consumers.remove(consumer);
+    }
+
+    public void notifyTextChanged(PrimitiveOperation operation, EditorState state) {
+        var info = new ContextRowNotificationModel();
+
+        var changedRowsIndexes = state.getChangedStorageRowIndexesWithClearing();
+        var rowsContent = new ArrayList<RowContentModel>();
+
+        for (var rowIndex : changedRowsIndexes) {
+            rowsContent.add(new RowContentModel(rowIndex, storageRowToString(state.getStorageRow(rowIndex))));
+        }
+
+        info.setOperation(operation);
+        info.setRowsContent(rowsContent);
+
+        consumers.forEach(m -> m.setInfo(info));
+    }
+
+    public void notifyCursorChanged(PrimitiveOperation operation, EditorState state) {
+        var info = new ContextCursorNotificationModel();
+
+        info.setOperation(operation);
+        info.setCursorColumnIndex(state.getCursorColumnIndex());
+        info.setCursorRowIndex(state.getCursorRowIndex());
+
+        consumers.forEach(m -> m.setInfo(info));
+    }
+
+    private String storageRowToString(Collection<Integer> row) {
+        StringBuilder str = new StringBuilder();
+        for (int ch : row) {
+            if (ch == AsciiConstant.ENTER) continue;
+            str.append((char) ch);
+        }
+
+        return str.toString();
+    }
+}
